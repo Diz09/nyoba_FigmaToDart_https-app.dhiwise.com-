bd48c38ebe8427bb6a472a011d0fe3bc4aabb25f
Dimas <124556325+Diz09@users.noreply.github.com>
2024-05-27 07:04:12 +0700

    add new presentation

---
+++ lib/presentation/container_screen/container_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../../core/app_export.dart';
+import '../../widgets/custom_bottom_bar.dart';
+import '../home_page/home_page.dart';
+import '../keranjang_page/keranjang_page.dart';
+import '../menu_base_page/menu_base_page.dart';
+import '../setting_page/setting_page.dart';
+
---
+++ lib/presentation/keranjang_page/keranjang_page.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import '../../core/app_export.dart';
+import '../../widgets/custom_bottom_bar.dart';
+import '../home_container_screen/home_container_page.dart';
